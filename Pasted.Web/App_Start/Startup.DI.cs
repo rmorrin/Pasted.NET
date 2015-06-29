3e8fa1708ebe24efa4b6aeccd839d67d6485e31e
Ryan Morrin <ryanmorrin@gmail.com>
2015-06-29 22:51:55 +0100

    Install and configure SimpleInjector for IoC

---
+++ Pasted.Web/App_Start/Startup.DI.cs
@@ -0,0 +1,48 @@
+﻿using Owin;
+using Pasted.DataAccess;
+using SimpleInjector;
+using SimpleInjector.Extensions;
+using SimpleInjector.Integration.Web;
+using SimpleInjector.Integration.Web.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Pasted.Web
+{
+    public partial class Startup
+    {
+        public void ConfigureDependencyInjection(IAppBuilder app)
+        {
+            // Create a new Simple Injector container
+            var container = new Container();
+
+            // Configure the container (register dependencies)
+            InitializeContainer(container);
+
+            // Register all MVC controllers
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+
+            // Optionally verify the container's configuration.
+            container.Verify();
+
+            // Register the container as MVC DependencyResolver.
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+        }
+
+        private static void InitializeContainer(Container container)
+        {
+            // DbContext, UoW & Repository
+            container.RegisterPerWebRequest<DbContext, PastedDbContext>();
+            container.RegisterPerWebRequest<IUnitOfWork, UnitOfWork>();
+            container.Register<IRepository, GenericRepository>();
+
+            // Services
+            // TODO
+        }
+    }
+}
--- Pasted.Web/Startup.cs
+++ Pasted.Web/Startup.cs
@@ -11,6 +11,7 @@ namespace Pasted.Web
         public void Configuration(IAppBuilder app)
         {
             // For more information on how to configure your application, visit http://go.microsoft.com/fwlink/?LinkID=316888
+            ConfigureDependencyInjection(app);
             ConfigureHangfire(app);
         }
     }
