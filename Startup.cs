89dbdcafe22141a066ddff5aa23014eb9ba27243
Ryan Morrin <ryanmorrin@gmail.com>
2017-04-18 10:06:27 +0100

    Migrated to new csproj format

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,7 @@ namespace Pasted
             if (env.IsDevelopment())
             {
                 // For more details on using the user secret store see https://go.microsoft.com/fwlink/?LinkID=532709
-                builder.AddUserSecrets();
+                builder.AddUserSecrets<Startup>();
             }
 
             builder.AddEnvironmentVariables();
